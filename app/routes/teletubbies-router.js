d22deae75cb27eb434803f9a4d4cfb8140d10e8b
CarolPC <carolina.pascale.c@gmail.com>
2017-07-03 23:43:07 -0300

    Add a post route to create a teletubbie

--- app/routes/teletubbies-router.js
+++ app/routes/teletubbies-router.js
@@ -2,5 +2,6 @@ const teletubbies = require('express').Router();
 const controller = require('../controllers/teletubbies-controller');
 
 teletubbies.get('/', controller.index);
+teletubbies.post('/', controller.create);
 
 module.exports = teletubbies;
